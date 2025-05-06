fdcfbfa8a82c6df5934e30e0b01a99bd433348d8
IamRam3 <sairamd1905@gmail.com>
2025-05-06 12:32:06 +0530

    checking order_history(BE) with client(FE)

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import AnalyticsReport from "./components/analyticsReport";
+
+export default function Home() {
+  return (
+    <div>
+      Stock Broker App
+      <AnalyticsReport />
+    </div>
+  );
+}
